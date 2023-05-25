5eeaaec9fcca80f5ecc660efbf13619dd95554f3
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-26 00:30:39 +0100

    0x00 - Add task 6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Tools for handling passwords."""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hash a user's password."""
+    return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a password is valid."""
+    return bcrypt.checkpw(password, hash_password)
