c9871d5414cb85ab81bdb038fe0b9a8a48eea9ee
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-25 20:34:24 +0100

    0x00 - Fix task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,8 +31,9 @@ def get_logger() -> logging.Logger:
     """Create a logger."""
     logger = logging.getLogger(name="user_data")
     logger.setLevel(logging.INFO)
-    logger.addHandler(StreamHandler())
-    logger.setFormatter(RedactingFormatter(PII_FIELDS))
+    handler = StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
     return logger
 
 
