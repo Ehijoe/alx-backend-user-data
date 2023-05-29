67ffe97b2efcdcb169111a9f91954829b597956e
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-29 21:12:22 +0100

    0x01 - Add basic authentication class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Authentication system."""
 from typing import List, TypeVar
-from flask import request
 
 
 class Auth:
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Basic authentication."""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class."""
