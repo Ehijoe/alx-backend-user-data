68864773584c1873d0c159e8b30590366da434a8
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-29 21:22:47 +0100

    0x01 - Add extraction of base64 string to basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic authentication class."""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract base64 string from Authorization header."""
+        if type(authorization_header) is not str:
+            return None
+        elif authorization_header[:6] == "Basic ":
+            return authorization_header[6:]
+        else:
+            return None
