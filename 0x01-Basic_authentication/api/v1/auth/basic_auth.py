b6dd78114e11a379aaf86cb5cdef3fbf936968c7
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-29 21:55:34 +0100

    0x01 - Add method for base64 decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Basic authentication."""
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -14,4 +16,17 @@ class BasicAuth(Auth):
         elif authorization_header[:6] == "Basic ":
             return authorization_header[6:]
         else:
+            return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """Decode a base64 string."""
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header,
+                                    validate=True).decode("utf-8")
+        except (binascii.Error, UnicodeDecodeError):
             return None
