eafb0b703c4ebd20fed4b73f54d33142b0d4727a
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-29 10:21:26 +0100

    0x01 - Implement Auth require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,11 @@ class Auth:
     """Authentication class."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Apply authentication to certain paths."""
-        return False
+        """Check if a path requires authentication."""
+        if path is None or excluded_paths is None or len(path) == 0:
+            return True
+        new_path = path if path[-1] == "/" else path + "/"
+        return new_path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """To be implemented."""
