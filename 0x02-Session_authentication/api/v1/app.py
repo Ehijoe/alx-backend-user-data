c194c6c9c86987c0eee05601872d579cdce775c9
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-01 14:42:42 +0100

    0x02 - Allow getting current user

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,10 +30,11 @@ def check_auth():
                       '/api/v1/forbidden/']
     if auth is None:
         return
-    elif auth.require_auth(request.path, excluded_paths):
+    request.current_user = auth.current_user(request)
+    if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
-        if auth.current_user(request) is None:
+        if request.current_user is None:
             abort(403)
 
 
