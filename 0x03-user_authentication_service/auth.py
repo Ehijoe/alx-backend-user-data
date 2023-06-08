b459f4fc04bd9da18f8cf8e00b1aaadb72700bde
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 16:29:27 +0100

    0x03 - Add function to generate uuids

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ class Auth:
         """Register a user."""
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
