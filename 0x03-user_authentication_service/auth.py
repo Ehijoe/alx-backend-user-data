e958c8fc484ce404232f5b4bba99729b23e04cc7
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 16:34:28 +0100

    0x03 - Add method to create new sessions

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,6 +29,16 @@ class Auth:
             return False
         return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
 
+    def create_session(self, email):
+        """Create a new session."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password."""
