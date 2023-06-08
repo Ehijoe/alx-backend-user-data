36e1952d53a3adcf7c7fb439c9ea9edd82ec03fa
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 17:23:26 +0100

    0x03 - Add a method for generating password reset tokens

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,6 +53,16 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
+    def get_reset_password_token(self, email):
+        """Generate a password reset token for a user."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password."""
