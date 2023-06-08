4674d1f6e8d19c52e15a758fbb8e60905b5825cd
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 17:25:31 +0100

    0x03 - Add type annotations

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,7 +53,7 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
-    def get_reset_password_token(self, email):
+    def get_reset_password_token(self, email: str) -> str:
         """Generate a password reset token for a user."""
         try:
             user = self._db.find_user_by(email=email)
