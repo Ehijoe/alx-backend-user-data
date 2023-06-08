74a33e666f60e0a38f48f4edf65f220e9d941990
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 16:21:37 +0100

    0x03 - Add method to validate user credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,6 +20,14 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
+    def valid_login(self, email, password):
+        """Validate login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password."""
