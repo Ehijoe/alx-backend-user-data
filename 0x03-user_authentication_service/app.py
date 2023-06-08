d3176addc3eec2b2cf9163ed852cf2608f4d406c
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 17:13:52 +0100

    0x03 - Add a logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask application."""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -41,6 +41,17 @@ def login():
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """Log out a user."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == '__main__':
