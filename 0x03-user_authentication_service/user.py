1b3413b6c00e01ac7be01ff605e40c707d4e6c59
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 14:33:23 +0100

    0x03 - Change length of fields in User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,7 +12,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column("id", Integer, primary_key=True)
-    email = Column("email", String, nullable=False)
-    hashed_password = Column("hashed_password", String, nullable=False)
-    session_id = Column("session_id", String, nullable=True)
-    reset_token = Column("reset_token", String, nullable=True)
+    email = Column("email", String(250), nullable=False)
+    hashed_password = Column("hashed_password", String(250), nullable=False)
+    session_id = Column("session_id", String(250), nullable=True)
+    reset_token = Column("reset_token", String(250), nullable=True)
