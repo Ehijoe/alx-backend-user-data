e8f1a66c7d4187d7eab89def51138fa650db313d
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-08 16:57:47 +0100

    0x03 - Add method to get user from sessioin id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,6 +39,15 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
+    def get_user_from_session_id(self, session_id):
+        """Get a user from a session id."""
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password."""
